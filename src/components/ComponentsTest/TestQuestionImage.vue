165bde6248ed450f1668852fcd05cae6ab323ae2
excitexcite <excitenew@gmail.com>
2023-04-24 14:13:32 +0300

    Implement correct approach for retriving images from source directory

--- src/components/ComponentsTest/TestQuestionImage.vue
+++ src/components/ComponentsTest/TestQuestionImage.vue
@@ -1,9 +1,13 @@
 <template>
   <div class="test__image-wrapper">
-    <img :src="`src/assets/${question.imgName}`" alt="Изображение для вопроса" />
+    <img :src="getImgUrl(question.imgName)" alt="Изображение для вопроса" />
   </div>
 </template>
 
 <script setup>
 defineProps(['question'])
+// const props = defineProps(['question'])
+
+const getImgUrl = (imageNameWithExtension) =>
+  new URL(`../../assets/${imageNameWithExtension}`, import.meta.url).href
 </script>
